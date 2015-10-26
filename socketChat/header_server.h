6fcd34aea1ee46a7bb825c37a858e4297f433850
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-10-25 21:50:13 -0700

    file structure changes

---
+++ socketChat/header_server.h
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <cstring>      // Needed for memset
+#include <sys/socket.h> // Needed for the socket functions
+#include <netdb.h>      // Needed for the socket functions
