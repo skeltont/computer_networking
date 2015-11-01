5954d7b4dcb35d35b2a708a8453b5fd8968b29be
Ty Skelton <skeltont@onid.oregonstate.edu>
2015-11-01 14:08:27 -0800

    fixed ANOTHER dependency issue

--- socketChat/header_server.h
+++ socketChat/header_server.h
@@ -3,7 +3,8 @@
 #include <sys/socket.h> // Needed for the socket functions
 #include <netdb.h>      // Needed for the socket functions
 #include <pthread.h>   	/* for threading , link with lpthread */
-#include <stdio.h>      // for printf and stuff 
+#include <stdio.h>      // for printf and stuff
+#include <cstdlib.h>    // because i like c better than c++
 
 # define PROMPT 		"> "
 # define QUIT   "/quit"
